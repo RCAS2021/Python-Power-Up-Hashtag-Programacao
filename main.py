7536e1c0e5cb57232a57b394d5fbecbdbb9e8428
Rafael Cruz <rcas2009@outlook.com>
2024-07-08 20:08:12 -0300

    Colocando Passo a Passo

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+# Passo 1: Entrar no sistema da empresa através do link
+# Link: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 2: Preencher login
+# Passo 3: Preencher senha
+# Passo 4: Clicar em logar
+# Passo 5: Importar a base de dados
+# Passo 6: Preencher dados
+# Passo 7: Clicar em cadastrar
+# Passo 8: Repetir passo 6 até cadastrar todos os produtos
+
