36bbdee5f363bcd5400e22bd1771edea4e10e143
Rafael Cruz <rcas2009@outlook.com>
2024-07-08 21:28:45 -0300

    Versão 1.0

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# Python Power Up-Hashtag Programacao
- Python Power Up - Automação de tarefas na Primeira Aula da Jornada Python da Hashtag Programação
+# Python Power Up - Hashtag Programacao
+    Python Power Up - Automação de tarefas na Primeira Aula da Jornada Python da Hashtag Programação
+
+# Desafio
+    Automatizar cadastro de produtos a partir de arquivo csv em sistema web, para ser executado diariamente e sob demanda.
+
+# Bibliotecas utilizadas
+- pyautogui
+- pandas
+- openpyxl
+- numpy
+- time
+
+# Arquivos
+- main.py: Realiza as automações
+- auxiliar.py: Descobre a posição dos campos na tela
+
+
+# Passos realizados
+- Passo 1: Entrar no sistema da empresa através do link
+    - Abrir o navegador
+        - Apertar tecla windows do computador
+        - Digitar Chrome
+        - Apertar enter
+    - Entrar no link https://dlp.hashtagtreinamentos.com/python/intensivao/login
+        - Digitar o link
+        - Apertar enter
+        - Aguardar o site
+
+- Passo 2: Fazer login
+    - Selecionar campo de email
+        - Selecionar tudo no campo caso haja auto preenchimento
+    - Digitar email
+    - Selecionar campo de senha
+        - Selecionar tudo no campo caso haja auto preenchimento
+    - Digitar senha
+    - Clicar no botão logar
+
+- Passo 3: Importar a base de dados
+    - Armazenar a base de dados em uma variável utilizando o pandas
+    
+- Passo 4: Cadastrar produto
+    - Selecionar campo código do produto
+    - Digitar código do produto
+    - Selecionar campo marca
+    - Digitar marca do produto
+    - Selecionar campo tipo
+    - Digitar tipo do produto
+    - Selecionar campo categoria
+    - Digitar categoria do produto
+    - Selecionar campo preço unitario
+    - Digitar preço unitário do produto
+    - Selecionar campo custo
+    - Digitar custo do produto
+    - Selecionar campo observações
+    - Digitar observações do produto
+    - Clicar no botão enviar
+    - Scrollar para o topo da página
+
+- Passo 5: Repetir passo 4 até cadastrar todos os produtos
+    - Adicionar estrutura de repetição for para passar por cada linha da base de dados, repetindo o passo 4
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pyautogui
+import pandas as pd
+import openpyxl
+import numpy
+import time
 
 # Passo 1: Entrar no sistema da empresa através do link
 # Link: https://dlp.hashtagtreinamentos.com/python/intensivao/login
@@ -10,3 +14,120 @@ import pyautogui
 # Passo 7: Clicar em cadastrar
 # Passo 8: Repetir passo 6 até cadastrar todos os produtos
 
+# Link do sistema
+link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+
+# Adicionando pausa entre os comandos para evitar erros
+pyautogui.PAUSE = 1
+
+# Passo 1:
+# Abrir o navegador
+# Apertar tecla windows do computador
+pyautogui.press('win')
+
+# Digitar chrome
+pyautogui.write('chrome')
+
+# Apertar enter
+pyautogui.press('enter')
+
+# Entrar no link
+# Digitar o link
+pyautogui.write(link)
+
+# Apertar enter
+pyautogui.press('enter')
+
+# Aguardar o site carregar
+time.sleep(3)
+
+# Passo 2:
+# Posições dos campos e botões
+# Posição do campo email
+pos_email = [1396, 471]
+
+# Posição do campo senha
+pos_senha = [1396, 604]
+
+# Posição do botão logar
+pos_logar = [1270, 674]
+
+# Fazer login
+# Selecionar campo de email
+pyautogui.click(x=pos_email[0], y=pos_email[1])
+
+# Prevenindo auto preenchimento
+# Selecionar tudo no campo -> ctrl + a
+pyautogui.hotkey('ctrl', 'a')
+
+# Digitar o email
+pyautogui.write('email@email.com')
+
+# Selecionar campo de senha
+pyautogui.click(x=pos_senha[0], y=pos_senha[1])
+
+# Selecionando tudo para evitar auto preenchimento
+pyautogui.hotkey('ctrl', 'a')
+
+# Digitar a senha
+pyautogui.write('Senha@123')
+
+# Clicar no botão logar
+pyautogui.click(x=pos_logar[0], y=pos_logar[1])
+
+# Aguardar a página carregar
+time.sleep(3)
+
+# Passo 3:
+# Importar a base de dados
+produtos = pd.read_csv('produtos.csv')
+
+# Passo 4 e 5:
+# Percorrendo as linhas da tabela
+for r in produtos.index:
+    # Cadastrar produto
+    # Posição do campo de código do produto
+    pos_cod = [1343, 320]
+
+    # Código
+    # Selecionar o campo código do produto
+    pyautogui.click(x=pos_cod[0], y=pos_cod[1])
+
+    # Digitar código
+    pyautogui.write(produtos.codigo[r])
+    # Selecionar próximo campo
+    pyautogui.press('tab')
+
+    # Marca
+    pyautogui.write(produtos.marca[r])
+    pyautogui.press('tab')
+
+    # Tipo
+    pyautogui.write(produtos.tipo[r])
+    pyautogui.press('tab')
+
+    # Categoria
+    pyautogui.write(str(produtos.categoria[r]))
+    pyautogui.press('tab')
+
+    # Preço Unitário
+    pyautogui.write(str(produtos.preco_unitario[r]))
+    pyautogui.press('tab')
+
+    # Custo
+    pyautogui.write(str(produtos.custo[r]))
+    pyautogui.press('tab')
+
+    # Observações
+    pyautogui.write(str(produtos.obs[r]))
+    pyautogui.press('tab')
+
+    # Clicar no botão de enviar
+    pyautogui.press('enter')
+
+    # Voltar ao topo utilizando home
+    pyautogui.press('home')
+
+    # Outra opção -> voltar ao topo utilizando scroll
+    # pyautogui.scroll(-1000)
+  
